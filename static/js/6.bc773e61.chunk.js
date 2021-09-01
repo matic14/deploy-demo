00020879dfac142246bb5e3daac88af6d37b77fc
prashant-patidar <prashant.patidar1402@gmail.com>
2021-09-01 08:34:16 +0530

    Updates

---
+++ static/js/6.bc773e61.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdemo=this.webpackJsonpdemo||[]).push([[6],{804:function(n,o){},844:function(n,o){},912:function(n,o){},917:function(n,o){}}]);
+//# sourceMappingURL=6.bc773e61.chunk.js.map
